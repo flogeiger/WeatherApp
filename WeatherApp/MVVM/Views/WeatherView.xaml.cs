611ede00724f0685776853001cdda616dd2b7568
flo <florian.geiger@hexagonsi.com>
2023-11-29 17:51:02 +0100

    first commit

---
+++ WeatherApp/MVVM/Views/WeatherView.xaml.cs
@@ -0,0 +1,12 @@
+using WeatherApp.MVVM.ViewModels;
+
+namespace WeatherApp.MVVM.Views;
+
+public partial class WeatherView : ContentPage
+{
+	public WeatherView()
+	{
+		InitializeComponent();
+		BindingContext = new WeatherViewModel();
+	}
+}
