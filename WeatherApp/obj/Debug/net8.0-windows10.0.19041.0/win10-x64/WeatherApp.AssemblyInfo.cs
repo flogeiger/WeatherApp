dcbe9ced292addc23470fa0b921ae726ac0995f7
flo <florian.geiger@hexagonsi.com>
2023-11-29 18:23:44 +0100

    no clue

--- WeatherApp/obj/Debug/net8.0-windows10.0.19041.0/win10-x64/WeatherApp.AssemblyInfo.cs
+++ WeatherApp/obj/Debug/net8.0-windows10.0.19041.0/win10-x64/WeatherApp.AssemblyInfo.cs
@@ -18,7 +18,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WeatherApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+d4b0a7aa7461e06d27926042877514490adbc965")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("WeatherApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WeatherApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
